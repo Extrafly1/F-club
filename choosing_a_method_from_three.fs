c2ca2941ad3bcc97c07d6cdb713ca01e1c34de7f
Efl <133080730+Extrafly1@users.noreply.github.com>
2025-04-03 00:12:06 +0300

    lab5_20 choosing_a_method_from_three.fs

---
+++ choosing_a_method_from_three.fs
@@ -0,0 +1,89 @@
+open System
+
+let rec private gcd a b =
+    match b with
+    | 0 -> abs a
+    | _ -> gcd b (a % b)
+
+let isPrime x =
+    let rec checkDivisor d =
+        match d with
+        | d when d * d > x -> true
+        | d when x % d = 0 -> false
+        | _ -> checkDivisor (d + 1)
+    match x with
+    | x when x < 2 -> false
+    | _ -> checkDivisor 2
+
+let sumDigits n =
+    let rec loop x acc =
+        match x with
+        | 0 -> acc
+        | _ -> loop (x / 10) (acc + abs(x) % 10)
+    loop (abs n) 0
+
+let processDivisors n initValue operation predicate =
+    let rec loop d acc =
+        match d with
+        | d when d > abs n -> acc
+        | d when n % d = 0 -> 
+            loop (d + 1) (operation acc (d, predicate d))
+        | _ -> loop (d + 1) acc
+    loop 1 initValue
+
+// Метод 1: Сумма простых делителей
+let sumPrimeDivisors n =
+    let folder acc (d, isPrime) = 
+        match isPrime with
+        | true -> acc + d
+        | false -> acc
+    processDivisors n 0 folder isPrime
+
+// Метод 2: Количество нечётных цифр > 3
+let countOddDigitsOver3 n =
+    let rec processDigits num acc =
+        match num with
+        | 0 -> acc
+        | _ ->
+            let digit = abs(num) % 10
+            processDigits (num / 10) (acc + 
+                match digit % 2, digit > 3 with
+                | 1, true -> 1
+                | _, _ -> 0)
+    processDigits (abs n) 0
+
+// Метод 3: Произведение делителей с суммой цифр < исходной
+let productSpecialDivisors n =
+    let targetSum = sumDigits n
+    let predicate d = sumDigits d < targetSum
+    let folder acc (d, isValid) =
+        match isValid with
+        | true -> acc * d
+        | false -> acc
+    match n with
+    | 0 -> 0
+    | _ -> processDivisors n 1 folder predicate
+  
+let getOperation = function
+    | 1 -> sumPrimeDivisors
+    | 2 -> countOddDigitsOver3 
+    | 3 -> productSpecialDivisors
+    | _ -> (fun _ -> -1)
+
+let parseInput (input:string) =
+    let parts = input.Split()
+    (int parts.[0], int parts.[1])
+
+[<EntryPoint>]
+let main argv =
+    (fun () -> Console.ReadLine())
+    >> parseInput
+    >> (fun (opId, arg) -> (getOperation opId) arg)
+    >> printfn "Результат: %d"
+    |> fun f -> f()
+
+    Console.ReadLine()
+    |> parseInput                       
+    |> fun (opId, arg) -> getOperation opId arg           
+    |> printfn "Результат: %d"  
+    0
