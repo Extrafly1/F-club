312b2a7bce3191c7b85174cf5b7d36bf918d7cfd
Efl <133080730+Extrafly1@users.noreply.github.com>
2025-04-02 22:44:08 +0300

    lab5_14 coprime_numbers and euler function

--- coprime_numbers.fs
+++ coprime_numbers.fs
@@ -17,10 +17,20 @@ let processCoprimes n (f:int->int->int) acc =
     | n when (abs n) < 2 -> acc
     | _ -> loop 1 acc
 
+let eulerPhi n =
+    match n with
+    | n when (abs n) = 1 -> 1
+    | _ -> processCoprimes (abs n) (fun acc _ -> acc + 1) 0
+
 [<EntryPoint>]
 let main argv =
     printfn "%d" (processCoprimes 6 (+) 0)
     printfn "%d" (processCoprimes 9 (*) 1)
     printfn "%d" (processCoprimes 3 (fun acc x -> if acc < x then acc else x) 10)
     printfn "%d" (processCoprimes 7 (fun acc _ -> acc + 1) 0)
+    printfn "1 -> %d" (eulerPhi 1)
+    printfn "7 -> %d" (eulerPhi 7)
+    printfn "9 -> %d" (eulerPhi 9)
+    printfn "6 -> %d" (eulerPhi 6)
+    printfn "0 -> %d" (eulerPhi 0)
     0
