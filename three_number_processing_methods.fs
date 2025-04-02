6c58fabedbcbd298086c2dc3ddc22698ef73f2bc
Efl <133080730+Extrafly1@users.noreply.github.com>
2025-04-02 23:23:54 +0300

    lab5_16_2 three_number_processing_methods

--- three_number_processing_methods.fs
+++ three_number_processing_methods.fs
@@ -20,7 +20,21 @@ let sumPrimeDivisors n =
     | 0 -> 0
     | _ -> loop 1 0
 
+// Метод 2: Количество нечетных цифр > 3
+let countOddDigitsOver3 n =
+    let rec loop num acc =
+        match num with
+        | 0 -> acc
+        | _ ->
+            let digit = abs(num) % 10
+            loop (num / 10) (acc + 
+                match digit % 2, digit > 3 with
+                | 1, true -> 1
+                | _, _ -> 0)
+    loop (abs n) 0
+
 [<EntryPoint>]
 let main argv =
     printfn "Метод 1: %i (28 -> 2+7=9)" (sumPrimeDivisors 28)
+    printfn "Метод 2: %i (496837 -> 9,7 → 2)" (countOddDigitsOver3 496837)
     0
