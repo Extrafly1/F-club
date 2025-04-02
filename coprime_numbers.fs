40a4f2868ff7abf0f5eb351211da6a2e6febb6a6
Efl <133080730+Extrafly1@users.noreply.github.com>
2025-04-02 22:52:59 +0300

    lab5_15 coprime_numbers euler coprime_numbers_whit

--- coprime_numbers.fs
+++ coprime_numbers.fs
@@ -22,6 +22,26 @@ let eulerPhi n =
     | n when (abs n) = 1 -> 1
     | _ -> processCoprimes (abs n) (fun acc _ -> acc + 1) 0
 
+let processCoprimesWith n (f:int->int->int) i (y:int->bool) =
+    let rec gcd a b =
+        match b with
+        | 0 -> abs a
+        | _ -> gcd b (a % b)
+    
+    let rec loop c acc =
+        match c with
+        | c when c >= abs n -> acc
+        | _ ->
+            let newAcc = 
+                if gcd c (abs n) = 1 && y c 
+                then f acc c 
+                else acc
+            loop (c + 1) newAcc
+    
+    match n with
+    | n when (abs n) < 2 -> i
+    | _ -> loop 1 i
+
 [<EntryPoint>]
 let main argv =
     printfn "%d" (processCoprimes 6 (+) 0)
@@ -33,4 +53,8 @@ let main argv =
     printfn "9 -> %d" (eulerPhi 9)
     printfn "6 -> %d" (eulerPhi 6)
     printfn "0 -> %d" (eulerPhi 0)
+    printfn "%d" (processCoprimesWith 10 (+) 0 (fun x -> x % 2 = 0))
+    printfn "%d" (processCoprimesWith 15 (fun acc _ -> acc + 1) 0 (fun x -> x > 5))
+    printfn "%d" (processCoprimesWith 9 (fun acc x -> min acc x) 10 (fun x -> x % 2 = 0))
+    printfn "%d" (processCoprimesWith 7 (+) 0 (fun x -> x < 5))
     0
