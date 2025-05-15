62e91c303c991d97959707b53b1ece18fc8a673a
Efl <133080730+Extrafly1@users.noreply.github.com>
2025-05-15 16:51:10 +0300

    Update individualtask.fs

--- individualtask.fs
+++ individualtask.fs
@@ -1,23 +1,37 @@
 open System
 
 let calculatePrizeFund turns =
-    let finalProbs =
-        (1, [|1.0|]) 
-        |> Seq.unfold (fun (i, state) ->
-            if i > turns then None
-            else
-                let nextState = 
-                    [| for j in 0..i do
-                        let p = 1.0 / float (i + 1)
-                        let fromBlue = if j > 0 then state.[j-1] * p else 0.0
-                        let fromRed = if j < state.Length then state.[j] * (1.0 - p) else 0.0
-                        fromBlue + fromRed |]
-                Some(nextState, (i + 1, nextState))
-        )
-        |> Seq.last
-    
-    let requiredBlue = (turns / 2) + 1
-    let probability = finalProbs.[requiredBlue..] |> Array.sum
+    let rec loop currentTurn state =
+        match currentTurn > turns with
+        | true -> state
+        | false ->
+            let rec buildNextState j acc =
+                match j > currentTurn with
+                | true -> List.rev acc
+                | false ->
+                    let p = 1.0 / float (currentTurn + 1)
+                    let fromBlue = 
+                        match j > 0 with
+                        | true -> List.item (j-1) state * p
+                        | false -> 0.0
+                    let fromRed = 
+                        match j < List.length state with
+                        | true -> List.item j state * (1.0 - p)
+                        | false -> 0.0
+                    buildNextState (j+1) (fromBlue + fromRed :: acc)
+            
+            let nextState = buildNextState 0 []
+            loop (currentTurn + 1) nextState
+
+    let rec sumProbabilities lst count =
+        match lst, count with
+        | [], _ -> 0.0
+        | _, 0 -> List.sum lst
+        | x::xs, _ -> sumProbabilities xs (count - 1)
+
+    let finalProbs = loop 1 [1.0]
+    let requiredBlue = turns / 2 + 1
+    let probability = sumProbabilities finalProbs requiredBlue
     1.0 / probability |> floor |> int
 
 let result = calculatePrizeFund 15
